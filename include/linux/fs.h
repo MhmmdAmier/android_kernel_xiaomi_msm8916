05d93f55e2a677e0fd391b35d712258029e31715
Al Viro <viro@zeniv.linux.org.uk>
2013-06-19 15:26:04 +0400

    constify rw_verify_area()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1978,7 +1978,6 @@ extern int generic_update_time(struct inode *, struct timespec *, int);
 extern struct kobject *fs_kobj;
 
 #define MAX_RW_COUNT (INT_MAX & PAGE_CACHE_MASK)
-extern int rw_verify_area(int, struct file *, loff_t *, size_t);
 
 #define FLOCK_VERIFY_READ  1
 #define FLOCK_VERIFY_WRITE 2
