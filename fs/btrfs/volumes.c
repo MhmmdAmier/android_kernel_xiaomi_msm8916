66e15b23f1204f6e5a363a4335b3d25527dde3d3
Ilya Dryomov <idryomov@gmail.com>
2013-08-27 13:50:44 +0300

    Btrfs: stop refusing the relocation of chunk 0
    AFAICT chunk 0 is no longer special, and so it should be restriped just
    like every other chunk.  One reason for this change is us refusing the
    relocation can lead to filesystems that can only be mounted ro, and
    never rw -- see the bugzilla [1] for details.  The other reason is that
    device removal code is already doing this: it will happily relocate
    chunk 0 is part of shrinking the device.
    
    [1] https://bugzilla.kernel.org/show_bug.cgi?id=60594
    
    Reported-by: Xavier Bassery <xavier@bartica.org>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -3019,10 +3019,6 @@ again:
 		if (found_key.objectid != key.objectid)
 			break;
 
-		/* chunk zero is special */
-		if (found_key.offset == 0)
-			break;
-
 		chunk = btrfs_item_ptr(leaf, slot, struct btrfs_chunk);
 
 		if (!counting) {
@@ -3058,6 +3054,8 @@ again:
 			spin_unlock(&fs_info->balance_lock);
 		}
 loop:
+		if (found_key.offset == 0)
+			break;
 		key.offset = found_key.offset - 1;
 	}
 
