247fe150bcf301e0c2ac59f2f732d4aef70f4d85
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2014-05-06 09:26:30 -0400

    tracing: Add trace_<tracepoint>_enabled() function
    There are some code paths in the kernel that need to do some preparations
    before it calls a tracepoint. As that code is worthless overhead when
    the tracepoint is not enabled, it would be prudent to have that code
    only run when the tracepoint is active. To accomplish this, all tracepoints
    now get a static inline function called "trace_<tracepoint-name>_enabled()"
    which returns true when the tracepoint is enabled and false otherwise.
    
    As an added bonus, that function uses the static_key of the tracepoint
    such that no branch is needed.
    
      if (trace_mytracepoint_enabled()) {
    	arg = process_tp_arg();
    	trace_mytracepoint(arg);
      }
    
    Will keep the "process_tp_arg()" (which may be expensive to run) from
    being executed when the tracepoint isn't enabled.
    
    It's best to encapsulate the tracepoint itself in the if statement
    just to keep races. For example, if you had:
    
      if (trace_mytracepoint_enabled())
    	arg = process_tp_arg();
      trace_mytracepoint(arg);
    
    There's a chance that the tracepoint could be enabled just after the
    if statement, and arg will be undefined when calling the tracepoint.
    
    Link: http://lkml.kernel.org/r/20140506094407.507b6435@gandalf.local.home
    
    Acked-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/tracepoint.h
+++ include/linux/tracepoint.h
@@ -188,6 +188,11 @@ extern void syscall_unregfunc(void);
 	static inline void						\
 	check_trace_callback_type_##name(void (*cb)(data_proto))	\
 	{								\
+	}								\
+	static inline bool						\
+	trace_##name##_enabled(void)					\
+	{								\
+		return static_key_false(&__tracepoint_##name.key);	\
 	}
 
 /*
@@ -233,6 +238,11 @@ extern void syscall_unregfunc(void);
 	}								\
 	static inline void check_trace_callback_type_##name(void (*cb)(data_proto)) \
 	{								\
+	}								\
+	static inline bool						\
+	trace_##name##_enabled(void)					\
+	{								\
+		return false;						\
 	}
 
 #define DEFINE_TRACE_FN(name, reg, unreg)
