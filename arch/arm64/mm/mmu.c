0e607730da0c53874d4a3252c25b3e9d12dccd54
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2014-10-20 14:02:15 +0200

    arm64/mm: add explicit struct_mm argument to __create_mapping()
    Currently, swapper_pg_dir and idmap_pg_dir share the init_mm mm_struct
    instance. To allow the introduction of other pg_dir instances, for instance,
    for UEFI's mapping of Runtime Services, make the struct_mm instance an
    explicit argument that gets passed down to the pmd and pte instantiation
    functions. Note that the consumers (pmd_populate/pgd_populate) of the
    mm_struct argument don't actually inspect it, but let's fix it for
    correctness' sake.
    
    Change-Id: Iabd119a2c8ed0ae55839eb9c5376c5555597da6f
    Acked-by: Steve Capper <steve.capper@linaro.org>
    Tested-by: Leif Lindholm <leif.lindholm@linaro.org>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Git-commit: e1e1fddae74b72d0415965821ad00fe39aac6f13)
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [rvaswani@codeaurora.org: fixed merge conflicts]
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -282,9 +282,9 @@ pmdval_t get_pmd_prot_sect_kernel(unsigned long addr)
 }
 #endif
 
-static void __init alloc_init_pmd(pud_t *pud, unsigned long addr,
-				  unsigned long end, phys_addr_t phys,
-				  int map_io, bool pages)
+static void __init alloc_init_pmd(struct mm_struct *mm, pud_t *pud,
+				  unsigned long addr, unsigned long end,
+				  phys_addr_t phys, int map_io, bool pages)
 {
 	pmd_t *pmd;
 	unsigned long next;
@@ -305,7 +305,7 @@ static void __init alloc_init_pmd(pud_t *pud, unsigned long addr,
 	 */
 	if (pud_none(*pud) || pud_bad(*pud)) {
 		pmd = early_alloc(PTRS_PER_PMD * sizeof(pmd_t));
-		pud_populate(&init_mm, pud, pmd);
+		pud_populate(mm, pud, pmd);
 	}
 
 	pmd = pmd_offset(pud, addr);
@@ -329,16 +329,16 @@ static void __init alloc_init_pmd(pud_t *pud, unsigned long addr,
 	} while (pmd++, addr = next, addr != end);
 }
 
-static void __init alloc_init_pud(pgd_t *pgd, unsigned long addr,
-				  unsigned long end, unsigned long phys,
-				  int map_io, bool force_pages)
+static void __init alloc_init_pud(struct mm_struct *mm, pgd_t *pgd,
+				  unsigned long addr, unsigned long end,
+				  phys_addr_t phys, int map_io, bool force_pages)
 {
 	pud_t *pud;
 	unsigned long next;
 
 	if (pgd_none(*pgd)) {
 		pud = early_alloc(PTRS_PER_PUD * sizeof(pud_t));
-		pgd_populate(&init_mm, pgd, pud);
+		pgd_populate(mm, pgd, pud);
 	}
 	BUG_ON(pgd_bad(*pgd));
 
@@ -368,7 +368,7 @@ static void __init alloc_init_pud(pgd_t *pgd, unsigned long addr,
 				flush_tlb_all();
 			}
 		} else {
-			alloc_init_pmd(pud, addr, next, phys);
+			alloc_init_pmd(mm, pud, addr, next, phys, map_io, force_pages);
 		}
 		phys += next - addr;
 	} while (pud++, addr = next, addr != end);
@@ -378,9 +378,10 @@ static void __init alloc_init_pud(pgd_t *pgd, unsigned long addr,
  * Create the page directory entries and any necessary page tables for the
  * mapping specified by 'md'.
  */
-static void __init __create_mapping(pgd_t *pgd, phys_addr_t phys,
-				    unsigned long virt, phys_addr_t size,
-				    int map_io, bool force_pages)
+
+static void __init __create_mapping(struct mm_struct *mm, pgd_t *pgd,
+				    phys_addr_t phys, unsigned long virt,
+				    phys_addr_t size, int map_io, bool force_pages)
 {
 	unsigned long addr, length, end, next;
 
@@ -390,7 +391,7 @@ static void __init __create_mapping(pgd_t *pgd, phys_addr_t phys,
 	end = addr + length;
 	do {
 		next = pgd_addr_end(addr, end);
-		alloc_init_pud(pgd, addr, next, phys, map_io, force_pages);
+		alloc_init_pud(mm, pgd, addr, next, phys, map_io, force_pages);
 		phys += next - addr;
 	} while (pgd++, addr = next, addr != end);
 }
@@ -403,7 +404,8 @@ static void __init create_mapping(phys_addr_t phys, unsigned long virt,
 			&phys, virt);
 		return;
 	}
-	__create_mapping(pgd_offset_k(virt & PAGE_MASK), phys, virt, size, 0, force_pages);
+	__create_mapping(&init_mm, pgd_offset_k(virt & PAGE_MASK), phys, virt,
+			 size, 0, force_pages);
 }
 
 void __init create_id_mapping(phys_addr_t addr, phys_addr_t size, int map_io)
