deba664875b12c1a03e7504f255cb0c1dcd611fc
Rom Lemarchand <romlem@android.com>
2014-11-07 09:42:40 -0800

    memcg: add permission check
    Use the 'allow_attach' handler for the 'mem' cgroup to allow
    non-root processes to add arbitrary processes to a 'mem' cgroup
    if it has the CAP_SYS_NICE capability set.
    
    Bug: 18260435
    Change-Id: If7d37bf90c1544024c4db53351adba6a64966250
    Signed-off-by: Rom Lemarchand <romlem@android.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6133,6 +6133,12 @@ static int mem_cgroup_can_attach(struct cgroup_subsys_state *css,
 	return ret;
 }
 
+static int mem_cgroup_allow_attach(struct cgroup_subsys_state *css,
+				   struct cgroup_taskset *tset)
+{
+	return subsys_cgroup_allow_attach(css->cgroup, tset);
+}
+
 static void mem_cgroup_cancel_attach(struct cgroup_subsys_state *css,
 				     struct cgroup_taskset *tset)
 {
@@ -6298,6 +6304,11 @@ static void mem_cgroup_move_task(struct cgroup_subsys_state *css,
 #else	/* !CONFIG_MMU */
 static int mem_cgroup_can_attach(struct cgroup_subsys_state *css,
 				 struct cgroup_taskset *tset)
+{
+	return 0;
+}
+static int mem_cgroup_allow_attach(struct cgroup_subsys_state *css,
+				   struct cgroup_taskset *tset)
 {
 	return 0;
 }
