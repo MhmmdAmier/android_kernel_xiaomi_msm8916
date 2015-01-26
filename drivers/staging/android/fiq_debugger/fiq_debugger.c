18480cc593bf8c65a4780220041b63dbe254d2a2
Ruchi Kandoi <kandoiruchi@google.com>
2015-01-26 10:29:15 -0800

    Fix compile errors in accordance with changes from 3.14 to 3.18
    Signed-off-by: Ruchi Kandoi<kandoiruchi@google.com>

--- drivers/staging/android/fiq_debugger/fiq_debugger.c
+++ drivers/staging/android/fiq_debugger/fiq_debugger.c
@@ -538,7 +538,7 @@ static bool fiq_debugger_fiq_exec(struct fiq_debugger_state *state,
 					    state->current_cpu);
 	} else if (!strncmp(cmd, "cpu ", 4) && sysrq_on()) {
 		unsigned long cpu = 0;
-		if (strict_strtoul(cmd + 4, 10, &cpu) == 0)
+		if (kstrtoul(cmd + 4, 10, &cpu) == 0)
 			fiq_debugger_switch_cpu(state, cpu);
 		else
 			fiq_debugger_printf(&state->output, "invalid cpu\n");
