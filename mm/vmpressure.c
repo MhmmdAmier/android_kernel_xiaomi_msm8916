ce6e4ac1156519d1026904ac8c90cc93d3f77aed
nojick <nojick@gmail.com>
2021-03-19 03:54:45 +0300

    BACKPORT:3.18.cgroup: Merge branch 'memcg_event' into for-3.14
    cgroup: Merge branch 'memcg_event' into for-3.14
    
    Merge v3.12 based patch series to move cgroup_event implementation to
    memcg into for-3.14.  The following two commits cause a conflict in
    kernel/cgroup.c
    
      2ff2a7d ("cgroup: kill css_id")
      79bd981 ("cgroup, memcg: move cgroup_event implementation to memcg")
    
    Each patch removes a struct definition from kernel/cgroup.c.  As the
    two are adjacent, they cause a context conflict.  Easily resolved by
    removing both structs.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -428,7 +428,7 @@ void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio)
 /**
  * vmpressure_register_event() - Bind vmpressure notifications to an eventfd
  * @cg:		cgroup that is interested in vmpressure notifications
- * @cft:	cgroup control files handle
+ * @memcg:	memcg that is interested in vmpressure notifications
  * @eventfd:	eventfd context to link notifications with
  * @args:	event arguments (used to set up a pressure level threshold)
  *
@@ -438,13 +438,12 @@ void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio)
  * threshold (one of vmpressure_str_levels, i.e. "low", "medium", or
  * "critical").
  *
- * This function should not be used directly, just pass it to (struct
- * cftype).register_event, and then cgroup core will handle everything by
- * itself.
+ * To be used as memcg event method.
  */
-int vmpressure_register_event(struct cgroup *cg, struct cftype *cft,
+int vmpressure_register_event(struct mem_cgroup *memcg,
 			      struct eventfd_ctx *eventfd, const char *args)
 {
+	struct vmpressure *vmpr = memcg_to_vmpressure(memcg);
 	struct vmpressure *vmpr = cg_to_vmpressure(cg);
 	struct vmpressure_event *ev;
 	int level;
@@ -476,21 +475,19 @@ int vmpressure_register_event(struct cgroup *cg, struct cftype *cft,
 /**
  * vmpressure_unregister_event() - Unbind eventfd from vmpressure
  * @cg:		cgroup handle
- * @cft:	cgroup control files handle
+ * @memcg:	memcg handle
  * @eventfd:	eventfd context that was used to link vmpressure with the @cg
  *
  * This function does internal manipulations to detach the @eventfd from
  * the vmpressure notifications, and then frees internal resources
  * associated with the @eventfd (but the @eventfd itself is not freed).
  *
- * This function should not be used directly, just pass it to (struct
- * cftype).unregister_event, and then cgroup core will handle everything
- * by itself.
+ * To be used as memcg event method.
  */
-void vmpressure_unregister_event(struct cgroup *cg, struct cftype *cft,
+void vmpressure_unregister_event(struct mem_cgroup *memcg,
 				 struct eventfd_ctx *eventfd)
 {
-	struct vmpressure *vmpr = cg_to_vmpressure(cg);
+	struct vmpressure *vmpr = memcg_to_vmpressure(memcg);
 	struct vmpressure_event *ev;
 
 	if (!vmpr)
